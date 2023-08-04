756019e4899c8d665d91801d022914c876bd3909
GabrielG-tech <gabriel@mpsouza.com.br>
2023-08-04 12:20:37 -0300

    Desafio7.1

--- Desafios/Desafio6.java
+++ Desafios/Desafio6.java
@@ -6,6 +6,7 @@ public class Desafio6 {
     // Solicite ao usuário um valor entre 0 e 360º e converta-o para radianos.
     public static void main(String[] args) {
         double graus;
+        int voltas = 0;
         String continuar;
         Scanner inp = new Scanner(System.in);
         do {
@@ -14,30 +15,38 @@ public class Desafio6 {
                 graus = inp.nextDouble();
             } while (graus < 0);
 
-            if (graus > 360) {
-                do {
-                    graus -= 360;
-                } while (graus <= 360);
-            }
+            while (graus > 360) {
+                graus -= 360;
+                voltas += 1;
+            };
+
+            while (graus < 0) {
+                graus += 360;
+                voltas -= 1;
+            };
             
             if (graus % 180 == 0) {
                 System.out.print(graus + " para ");
                 System.out.printf("radianos: %.0f", Math.toRadians(graus));
             } else {
+                System.out.print(graus + " para ");
                 System.out.printf("radianos: %.2f", Math.toRadians(graus));
             }
-            
-            if (isInteger(Math.toRadians(graus)/Math.PI)) {
+            double grausPi = Math.toRadians(graus)/Math.PI;
+            if ( grausPi == (int) grausPi ) {
                 System.out.print("\n" + graus + " para ");
-                System.out.printf("PI: %.0fπ", (Math.toRadians(graus)/Math.PI));
+                System.out.printf("PI: %.0fπ", grausPi);
             } else {
                 System.out.print("\n" + graus + " para ");
-                System.out.printf("PI: %.2fπ", (Math.toRadians(graus)/Math.PI));
+                System.out.printf("PI: %.2fπ", grausPi);
             }
+
+            System.out.println("\nNúmero de voltas: " + voltas);
 
             System.out.print("\nDeseja continuar? (s/n): ");
             continuar = inp.next().toLowerCase();
-            while (continuar != "s" && continuar != "n") {
+
+            while (!continuar.equals("s") && !continuar.equals("n")) {
                 System.out.println("Digite 's' para sim ou 'n' para nao: ");
                 continuar = inp.next().toLowerCase();
             };
--- Desafios/Desafio7.java
+++ Desafios/Desafio7.java
@@ -1,9 +1,53 @@
 package Desafios;
 
+import java.util.Scanner;
 
 public class Desafio7 {
+    // Solicite ao usuário um valor em graus entre 0 e 360 e calcule o cosseno e seno.
     public static void main(String[] args) {
+        double graus; String continuar;
+        Scanner inp = new Scanner(System.in);
+        do {
+            do {
+                System.out.print("Informe um valor de ângulo (0° a 360°): ");
+                graus = inp.nextDouble();
+            } while (graus < 0);
 
+            while (graus > 360) {
+                graus -= 360;
+            };
+
+            while (graus < 0) {
+                graus += 360;
+            };
+            
+            // if (graus % 180 == 0) {
+            //     System.out.print(graus + " para ");
+            //     System.out.printf("radianos: %.0f", Math.toRadians(graus));
+            // } else {
+            //     System.out.print(graus + " para ");
+            //     System.out.printf("radianos: %.2f", Math.toRadians(graus));
+            // }
+            // double grausPi = Math.toRadians(graus)/Math.PI;
+            // if ( grausPi == (int) grausPi ) {
+            //     System.out.print("\n" + graus + " para ");
+            //     System.out.printf("PI: %.0fπ", grausPi);
+            // } else {
+            //     System.out.print("\n" + graus + " para ");
+            //     System.out.printf("PI: %.2fπ", grausPi);
+            // }
+
+            System.out.println("Seno: ");
+            System.out.println("Cosseno: ");
+
+            System.out.print("\nDeseja continuar? (s/n): ");
+            continuar = inp.next().toLowerCase();
+
+            while (!continuar.equals("s") && !continuar.equals("n")) {
+                System.out.println("Digite 's' para sim ou 'n' para nao: ");
+                continuar = inp.next().toLowerCase();
+            };
+        } while (continuar.equals("s"));
+        inp.close();
     }
-
 }
