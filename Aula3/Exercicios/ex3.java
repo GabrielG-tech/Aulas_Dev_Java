fc3f1d7ed51905b5c3a9741532e86ad7c1481a5e
Gabriel <99262377+GabrielG-tech@users.noreply.github.com>
2023-08-14 14:24:28 -0300

    TP1 - Java ex 3 e 4

--- Aula3/Exercicios/ex3.java
+++ Aula3/Exercicios/ex3.java
@@ -2,41 +2,37 @@ package Aula3.Exercicios;
 
 import java.util.Scanner;
 
-/*
-  3) Faça um  código para calcular a sequencia de Fibonacci e encontrar a razão áurea através dos 2 últimos valores da sequência. Solicite ao usuário um valor N para imprimir N elementos da sequência.
-*/
-
 public class ex3 {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int n;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
 
-    // Captura número de digitos para a sequência.
-    System.out.print("Digite o numero de digitos para a sequência: ");
-    n = input.nextInt();
-    input.close();
+        // Captura o número de dígitos para a sequência.
+        System.out.print("Digite o número de dígitos para a sequência: ");
+        n = input.nextInt();
+        input.close();
 
-    // Saída do resultado
-    System.out.println("Sequência Fibonacci com " + n + " digitos: ");
-    for (int i = 0; i <= n; i++) {
-      System.out.print("(" + i + "):" + Fibonacci(i) + "\t");
+        // Saída do resultado
+        System.out.println("Sequência Fibonacci com " + n + " dígitos: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("(" + i + "):" + Fibonacci(i) + "\t");
+        }
+        System.out.println("\nA Razão Áurea é " + calculoRazãoAurea(Fibonacci(n - 2), Fibonacci(n - 1)));
     }
-    System.out.println("\nA Razão Aurea é " + calculoRazãoAurea((n - 2), (n - 1)));
-  }
 
-  public static int Fibonacci(int n) {
-    // Calculo da sequência
-    if (n < 2) {
-      return n;
-    } else {
-      return (n - 1) + (n - 2);
+    public static int Fibonacci(int n) {
+        // Cálculo da sequência
+        if (n < 2) {
+            return n;
+        } else {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
     }
-  }
-  public static double calculoRazãoAurea(long a, long b) {
+
+    public static double calculoRazãoAurea(int a, int b) {
         if (b == 0) {
             return 0;
         }
-        return (double) a / b;
+        return (double) (a + b) / b;
     }
-  
 }
--- Aula3/Exercicios/ex4.java
+++ Aula3/Exercicios/ex4.java
@@ -1,9 +1,45 @@
 package Aula3.Exercicios;
 
+import java.util.Scanner;
+
 /*
   4) Solicite 3 lados de um triângulo a um usuário e diga se ele é: Triângulo escaleno (todos os lados diferentes), Triângulos isósceles (dois lados com medidas iguais) ou  Triângulo equilátero (todos os lados com iguais).
 */
 
 public class ex4 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o tamanho do primeiro lado do triângulo: ");
+        double lado1 = input.nextFloat();
+
+        System.out.print("Digite o tamanho do segundo lado do triângulo: ");
+        double lado2 = input.nextFloat();
+
+        System.out.print("Digite o tamanho do terceiro lado do triângulo: ");
+        double lado3 = input.nextFloat();
+
+        if (ehTriangulo(lado1, lado2, lado3)) {
+            String tipo = tipoTriangulo(lado1, lado2, lado3);
+            System.out.println("O triângulo é do tipo: " + tipo);
+        } else {
+            System.out.println("Os lados fornecidos não formam um triângulo.");
+        }
+
+        input.close();
+    }
+
+    public static boolean ehTriangulo(double a, double b, double c) {
+        return a + b > c && a + c > b && b + c > a;
+    }
+
+    public static String tipoTriangulo(double a, double b, double c) {
+        if (a == b && b == c) {
+            return "Triângulo equilátero";
+        } else if (a == b || a == c || b == c) {
+            return "Triângulo isósceles";
+        } else {
+            return "Triângulo escaleno";
+        }
+    }
 }
