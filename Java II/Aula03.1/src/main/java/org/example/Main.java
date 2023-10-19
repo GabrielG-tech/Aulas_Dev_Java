a6bde6eaadb396ffb2f82aa18a7ed4d53b91767a
GabrielG-tech <gabriel@mpsouza.com.br>
2023-10-19 11:31:30 -0300

    FreChart

---
+++ Java II/Aula03.1/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Casa k = new Casa();
+
+        Funcionário z = new Funcionário();
+
+        Random y = new Random();
+        int x = 0;
+
+        for (x = 0; x <= 10; x++)
+            System.out.println(x);
+    }
+}
