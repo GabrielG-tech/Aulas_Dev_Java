f492d8c45d447f79ea8964144233ebdf2e84d4ed
Gabriel <99262377+GabrielG-tech@users.noreply.github.com>
2023-11-07 02:10:54 -0300

    TesteCriptografia

--- Java II/Aula05.1_temperatura_umidade/src/test/java/org/example/MainTest.java
+++ Java II/Aula05.1_temperatura_umidade/src/test/java/org/example/MainTest.java
@@ -1,7 +1,60 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    @Test
+    void testEncriptografar() {
+        Criptografia criptografia = new Criptografia();
+        String mensagem = "HELLO";
+        int shift = 3;
+        String resultado = criptografia.encriptografar(mensagem, shift);
+        assertEquals("KHOOR", resultado);
+    }
 
+    @Test
+    void testDescriptografar() {
+        Criptografia criptografia = new Criptografia();
+        String mensagem = "KHOOR";
+        int shift = 3;
+        String resultado = criptografia.descriptografar(mensagem, shift);
+        assertEquals("HELLO", resultado);
+    }
+
+    @Test
+    void testEncriptografarComChaveNegativa() {
+        Criptografia criptografia = new Criptografia();
+        String mensagem = "HELLO";
+        int shift = -3;
+        String resultado = criptografia.encriptografar(mensagem, shift);
+        assertEquals("EBIIL", resultado);
+    }
+
+    @Test
+    void testDescriptografarComChaveNegativa() {
+        Criptografia criptografia = new Criptografia();
+        String mensagem = "EBIIL";
+        int shift = -3;
+        String resultado = criptografia.descriptografar(mensagem, shift);
+        assertEquals("HELLO", resultado);
+    }
+
+    @Test
+    void testEncriptografarComEspacosEOutrosCaracteres() {
+        Criptografia criptografia = new Criptografia();
+        String mensagem = "Hello, World!";
+        int shift = 3;
+        String resultado = criptografia.encriptografar(mensagem, shift);
+        assertEquals("Khoor, Zruog!", resultado);
+    }
+
+    @Test
+    void testDescriptografarComEspacosEOutrosCaracteres() {
+        Criptografia criptografia = new Criptografia();
+        String mensagem = "Khoor, Zruog!";
+        int shift = 3;
+        String resultado = criptografia.descriptografar(mensagem, shift);
+        assertEquals("Hello, World!", resultado);
+    }
 }
