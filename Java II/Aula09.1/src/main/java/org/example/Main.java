8b77f2fb6ea55bee1757d451db3dfc82ee10f02e
GabrielG-tech <gabriel@mpsouza.com.br>
2023-11-13 12:25:21 -0300

    WebSitePHP_Java_cor

--- Java II/Aula09.1/src/main/java/org/example/Main.java
+++ Java II/Aula09.1/src/main/java/org/example/Main.java
@@ -11,44 +11,48 @@ import java.io.IOException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        JFrame frame = new JFrame("Exemplo JComboBox");
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = screenSize.width / 2;
-        int height = screenSize.height;
-        frame.setSize(width, height);
-        frame.setLocation(screenSize.width / 2, 0);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        static JFrame frame = new JFrame("Cores");
 
-        JPanel panel = new JPanel();
-        frame.add(panel);
-
-        panel.setLayout(new FlowLayout());
-        final JPanel contentPanel = (JPanel)frame.getContentPane();
-        frame.setVisible(true);
-
-        while(true) {
+        public static void main(String[] args) throws InterruptedException {
             try {
-                String path = "https://sitetestegabrielsilveira.000webhostapp.com/arquivoTexto.txt";
-                Document doc = Jsoup.connect(path).get();
+                String path = "https://testeinfnet.000webhostapp.com//arquivo.txt";
+                int value = -1;
 
-                String corSelecionada = doc.body().ownText();
+                while (true) {
+                    Document doc = Jsoup.connect(path).get();
+                    int first = Integer.parseInt(doc.body().ownText());
 
-                if (corSelecionada.equals("0")) {
-                    contentPanel.setBackground(Color.GREEN);
-                } else if (corSelecionada.equals("1")) {
-                    contentPanel.setBackground(Color.YELLOW);
-                } else if (corSelecionada.equals("2")) {
-                    contentPanel.setBackground(Color.RED);
+                    if (first != value) {
+                        Janela(doc.body().ownText());
+                        value = Integer.parseInt(doc.body().ownText());
+                    }
+                    Thread.sleep(1000);
                 }
-                Thread.sleep(2000);
             } catch (IOException e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
         }
-    }
+        public static void Janela(String type) {
+            //frame.setSize(400, 400);
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            int width = screenSize.width / 2;
+            int height = screenSize.height / 2;
+            frame.setSize(width, height);
+            frame.setLocation(0, 0);
 
-//    public static void Cor() {
-//
-//    }
-}
+            switch (type) {
+                case "0":
+
+                    frame.getContentPane().setBackground(Color.GREEN);
+                    break;
+
+                case "1":
+                    frame.getContentPane().setBackground(Color.YELLOW);
+                    break;
+
+                case "2":
+                    frame.getContentPane().setBackground(Color.RED);
+            }
+            frame.setVisible(true);
+        }
+    }
